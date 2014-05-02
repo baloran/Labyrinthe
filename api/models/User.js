54e1c5becccf663253164e4255d1d52f48c43bc2
Baloran <contact@baloran.fr>
2014-05-02 22:47:34 +0200

    Mise en place BDD
    Mise en place de la base de donnée

--- api/models/User.js
+++ api/models/User.js
@@ -1,14 +1,55 @@
 /**
-* User.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
 
-  attributes: {
-
-  }
+	attributes: {
+		'mail': {
+			type: 'email',
+			required: true
+		},
+		'pseudo': {
+			type: 'string',
+			required: true
+		},
+		'password': {
+			type: 'password',
+			required: true
+		},
+		'exp': {
+			type: 'int',
+		},
+		'nbr_win': {
+			type: 'int',
+		},
+		'nbr_play': {
+			type: 'int',
+		},
+		'first_connection': {
+			type: 'datetime',
+		},
+		'avatar': {
+			type: 'string',
+		},
+		'id': {
+			type: 'string'
+		},
+		'save': {
+			'name': {
+				type: 'string',
+				required: true
+			},
+			'id': {
+				type: 'string',
+				required: true
+			},
+			'state': {
+				type: 'int'
+			}
+		}
+	}
 };
-
