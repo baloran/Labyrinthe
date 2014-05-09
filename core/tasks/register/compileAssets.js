d70d3b3bb92c177725b45565bc47a99ab901c06c
Baloran <contact@baloran.fr>
2014-05-09 12:36:33 +0200

    item, ennemy, ajout du design

--- core/tasks/register/compileAssets.js
+++ core/tasks/register/compileAssets.js
@@ -1,8 +1,9 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 	grunt.registerTask('compileAssets', [
 		'clean:dev',
 		'jst:dev',
 		'less:dev',
+		// 'compass:dev',
 		'copy:dev',
 		'coffee:dev'
 	]);
--- core/tasks/register/syncAssets.js
+++ core/tasks/register/syncAssets.js
@@ -1,7 +1,8 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 	grunt.registerTask('syncAssets', [
 		'jst:dev',
 		'less:dev',
+		// 'compass:dev',
 		'sync:dev',
 		'coffee:dev'
 	]);
